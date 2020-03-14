1b438b669d6fcb380358051b2950657634ddfe72
Rakib-RS <rakib.rscse@gmail.com>
2020-03-14 16:03:06 +0600

    connecting the event detailed page to the store

--- src/features/event/EventDetailed/EventDetailed.jsx
+++ src/features/event/EventDetailed/EventDetailed.jsx
@@ -4,33 +4,19 @@ import EventDetailedHeader from "./EventDetailedHeader";
 import EventDetailedInfo from "./EventDetailedInfo";
 import EventDetailedChat from "./EventDetailedChat";
 import EventDetailedSidebar from "./EventDetailedSidebar";
+import {connect} from 'react-redux'
+const mapState = (state, ownProps) =>{
+  const eventId = ownProps.match.params.id;
+  let event = {};
+  if(eventId && state.events.length >0){
+    event = state.events.filter( event => event.id === eventId)[0];
+  }
+  return {
+    event
+  }
+}
 
-const event = {
-  id: "1",
-  title: "Trip to Tower of London",
-  date: "2018-03-27",
-  category: "culture",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
-  city: "London, UK",
-  venue: "Tower of London, St Katharine's & Wapping, London",
-  hostedBy: "Bob",
-  hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
-  attendees: [
-    {
-      id: "a",
-      name: "Bob",
-      photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
-    },
-    {
-      id: "b",
-      name: "Tom",
-      photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
-    }
-  ]
-};
-
-const EventDetailed = () => {
+const EventDetailed = ({event}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
@@ -45,4 +31,4 @@ const EventDetailed = () => {
   );
 };
 
-export default EventDetailed;
+export default connect(mapState) (EventDetailed);
